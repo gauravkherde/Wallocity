c038c9d5df4934d133ea04e760ebcec95b041946
Gaurav <gauravkherde145@gmail.com>
2022-12-19 21:49:16 +0530

    added categories section

--- app/src/main/java/com/gaurav145/wallocity/activities/MainActivity.kt
+++ app/src/main/java/com/gaurav145/wallocity/activities/MainActivity.kt
@@ -3,10 +3,13 @@ package com.gaurav145.wallocity.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.gaurav145.wallocity.R
 import com.gaurav145.wallocity.retrofit.WallpaperRepository
 import com.gaurav145.wallocity.viewModel.HomeViewModel
 import com.gaurav145.wallocity.viewModel.WallpaperViewModelFactory
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     val viewModel :HomeViewModel by lazy {
@@ -19,5 +22,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNav)
+        val navController = Navigation.findNavController(this, R.id.mainFragment)
+        NavigationUI.setupWithNavController(bottomNavigation, navController )
     }
 }
---
+++ app/src/main/java/com/gaurav145/wallocity/fragment/CategoriesFragment.kt
@@ -0,0 +1,95 @@
+package com.gaurav145.wallocity.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.gaurav145.wallocity.R
+import com.gaurav145.wallocity.activities.SetWallpaper
+import com.gaurav145.wallocity.adapter.WallpaperAdapter
+import com.gaurav145.wallocity.databinding.FragmentCategoriesBinding
+
+
+class CategoriesFragment : Fragment() {
+    lateinit var binding: FragmentCategoriesBinding
+    private val bundle = Bundle()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentCategoriesBinding.inflate(inflater)
+        onCardsClick()
+        return binding.root
+    }
+
+    private fun onCardsClick() {
+        binding.fourCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "4K")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+
+        }
+        binding.animalCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "Animal")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.ArtCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "Art")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.BuildingCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "Building")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.CarCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "Car")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.CityScapeCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "City Scape")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.LightCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "Light")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.LoveCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "Love")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.MotorCycleCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "bike")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.PeopleCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "People")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.SpaceCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "Space")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.TechnologyCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "Technology")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+        binding.WordCard.setOnClickListener {
+            bundle.putString(CAT_NAME, "Word")
+            findNavController().navigate(R.id.action_categoriesFragment_to_wallpaperFragment,bundle)
+        }
+
+    }
+
+    companion object {
+        const val CAT_NAME = "CAT_NAME"
+    }
+}
